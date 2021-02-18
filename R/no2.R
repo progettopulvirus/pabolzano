6cebc6b0d0849ae7efec0217392406539ac34604
guidofioravanti <guidofioravanti@yahoo.it>
2021-02-18 01:56:08 +0100

    Eliminati duplicati PM10 e aggiunti inquinanti co, o3

--- R/no2.R
+++ R/no2.R
@@ -1,6 +1,6 @@
 #' Tibble con i dati di no2 per 5 stazioni della regione PABOLZANO
 #'
-#' @format Un tibble con 8 colonne e 13700 osservazioni
+#' @format Un tibble con 8 colonne e 13688 osservazioni
 #'
 #' @usage
 #' no2
---
+++ R/o3_max_mm8h_d.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di o3_max_mm8h_d per 4 stazioni della regione PABOLZANO
+#'
+#' @format Un tibble con 8 colonne e 10952 osservazioni
+#'
+#' @usage
+#' o3_max_mm8h_d
+"o3_max_mm8h_d"
