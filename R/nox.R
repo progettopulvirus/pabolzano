4f254486e513bb943ad6e1fffb4ac9882b595dce
guidofioravanti <guidofioravanti@yahoo.it>
2021-10-11 15:17:40 +0200

    Cambiati i criteri di selezione per le serie, adottati criteri utili al modello INLA sulle differenze

---
+++ R/nox.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di nox per 8 stazioni della regione PABOLZANO
+#'
+#' @format Un tibble con 8 colonne e 5848 osservazioni
+#'
+#' @usage
+#' nox
+"nox"
---
+++ R/pm10.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di pm10 per 5 stazioni della regione PABOLZANO
+#'
+#' @format Un tibble con 8 colonne e 3655 osservazioni
+#'
+#' @usage
+#' pm10
+"pm10"
